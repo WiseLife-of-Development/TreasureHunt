a5ed0e3325a5e75cff186367c7f24cfc7bc7b0fe
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2021-09-25 15:05:01 +0900

    reree

--- TreasureHunt/TreasureHunt/urls.py
+++ TreasureHunt/TreasureHunt/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from Main.views import *
+from recruitment.views import *
+from travel.views import *
+from myyolo.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
